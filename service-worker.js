0e226abdbcfe0d4985a0a2a1463ca19b34ff5c45
danielleao1982-dev <danielleao1982@gmail.com>
2025-10-03 20:48:33 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,26 @@
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('realmind-store').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/login.html',
-        '/cadastro.html',
-        '/menu.html',
-        '/teste.html',
-        '/relaxamento.html',
-        '/jogos.html',
-        '/diario.html',
-        '/apoio.html',
-        '/manifest.json'
-      ]);
-    })
-  );
+const CACHE = 'realmind-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './login.html',
+  './cadastro.html',
+  './menu.html',
+  './teste.html',
+  './relaxamento.html',
+  './jogos.html',
+  './diario.html',
+  './apoio.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
